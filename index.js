6f52cac3f70e356ec998240b2d7542466e143de6
priyanshu999pandey <priyanshu999pandey@gmail.com>
2025-11-17 22:43:15 +0530

    fix: cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import subCategoryRouter from "./routes/subCategory.route.js";
 
 const app = express();
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
+    origin:"*",
     credentials:true,
 }))
 app.use(express.json())
